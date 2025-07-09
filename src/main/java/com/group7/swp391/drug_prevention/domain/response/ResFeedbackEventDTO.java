6c896977421604b6cedab1efd5ce30588bc4a339
Nguyen Minh Nguyen <your_email@example.com>
2025-07-09 16:16:21 +0700

    add feedbackEventController

--- src/main/java/com/group7/swp391/drug_prevention/domain/response/ResFeedbackEventDTO.java
+++ src/main/java/com/group7/swp391/drug_prevention/domain/response/ResFeedbackEventDTO.java
@@ -1,21 +1,12 @@
 package com.group7.swp391.drug_prevention.domain.response;
 
-import com.group7.swp391.drug_prevention.domain.Event;
-import com.group7.swp391.drug_prevention.domain.User;
-import jakarta.persistence.Column;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.Data;
 
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
+@Data
 public class ResFeedbackEventDTO {
-    private int rating;
-    @Column(name = "comment",columnDefinition = "NVARCHAR(250)")
+    private Integer id;
+    private Integer eventId;
+    private Long memberId;
+    private Integer rating;
     private String comment;
-    private User member;
-    private Event event;
 }
